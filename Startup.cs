431f7730f0da5e19421463b136c119cbb3162d4a
Romero <rrcb@cin.ufpe.br>
2021-02-09 12:23:58 -0300

    Adding Siglenton for mongoDB

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace Api_Mongodb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<Data.MongoDB>(); // Singleton is not very good for real-world applications
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
